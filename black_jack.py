ac44b2b4385aacc0b01be6afa0ea2267327e9edd
el-gato-afk <155203553+elgatoafk@users.noreply.github.com>
2024-03-06 02:36:25 +0200

    Update black_jack.py

--- black_jack.py
+++ black_jack.py
@@ -36,7 +36,7 @@ def higher_card(card_one, card_two):
         return card_one
     if value_of_card(card_one) < value_of_card(card_two):
         return card_two
-    return tuple(card_one, card_two)
+    return card_one, card_two
 
 
 def value_of_ace(card_one, card_two):
@@ -52,7 +52,7 @@ def value_of_ace(card_one, card_two):
     sum_value = value_of_card(card_one) + value_of_card(card_two)
 
     if card_one == "A" or card_two == "A":
-        return 11
+        return 1
     return 1 if sum_value > 10 else 11
 
 
